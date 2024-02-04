0247a61b86389b92b1db7ef88bff2636b88643e2
TakiMoysha <mpwema782@gmail.com>
2024-02-04 01:56:55 +0100

    up

---
+++ doc/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"main":{"doc":"Testing documentation for repository","t":"A","n":["commands"],"q":[[0,"main"]],"d":[""],"i":[0],"f":[0],"c":[],"p":[],"b":[]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+
+//! Testing documentation for repository
+//! ## Example
+//! ```rust
+//! for arg in env::args() {
+//!     println!("{}", arg);
+//! }
+//! ```
+pub mod commands {}
+
 fn main() {
     println!("Taki-CLI")
 }
